e77ade4b1129718dd03e18ba44a5802f4818edc0
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-03-25 17:40:25 +0200

    u

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,13 +1,16 @@
 export default function Heading({
   className,
   title,
+  text,
 }: {
-  className: string;
-  title: string;
+  className?: string;
+  title?: string;
+  text?: string;
 }) {
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
       {title && <h2 className="h2 text-center">{title}</h2>}
+      {text && <p className="body-2 mt-5 text-n-4 text-center">{text}</p>}
     </div>
   );
 }
