5d6e3b0e4f4a27727999f695a3931b4bb1d1641e
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-03-26 00:31:32 +0200

    u

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,16 +1,23 @@
+import Tagline from "./Tagline";
+
 export default function Heading({
   className,
   title,
   text,
+  tag,
 }: {
   className?: string;
   title?: string;
   text?: string;
+  tag?: string;
 }) {
   return (
-    <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
+    <div
+      className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
+    >
       {title && <h2 className="h2 text-center">{title}</h2>}
       {text && <p className="body-2 mt-5 text-n-4 text-center">{text}</p>}
+      {tag && <Tagline className="mb-4 md:justify-center">{tag}</Tagline>}
     </div>
   );
 }
