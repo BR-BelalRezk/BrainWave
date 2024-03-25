fe3db9f2608343deee4f9bfef82e8a66a1f4c562
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-03-25 17:06:37 +0200

    Update Heading.tsx

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -7,7 +7,7 @@ export default function Heading({
 }) {
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
-      {title && <h2 className="h2">{title}</h2>}
+      {title && <h2 className="h2 text-center">{title}</h2>}
     </div>
   );
 }
